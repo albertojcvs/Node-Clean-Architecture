eaa06a41d7798e9ffde48c3a5f2cf7880205c991
Alberto Jose <albertojcvs@gmail.com>
2022-01-19 15:48:33 -0300

    refactor: move a server errot to helper method

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,7 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
-import { ServerError } from '../errors/server-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
@@ -19,10 +18,7 @@ export class SingUpController implements Controller {
 
       if (!isValid) return badRequest(new InvalidParamError('email'))
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
