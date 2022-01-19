2e013733f8983593482b91277943c0d21048636c
Alberto Jose <albertojcvs@gmail.com>
2022-01-19 17:30:46 -0300

    refactor: change the message of invalid param error

--- src/presentation/errors/invalid-param-error.ts
+++ src/presentation/errors/invalid-param-error.ts
@@ -1,6 +1,6 @@
 export class InvalidParamError extends Error {
   constructor (invalidParam: string) {
-    super(`Missing param: ${invalidParam}`)
-    this.name = 'MissingParamError'
+    super(`InalidParam: ${invalidParam}`)
+    this.name = 'InvalidParamError'
   }
 }
