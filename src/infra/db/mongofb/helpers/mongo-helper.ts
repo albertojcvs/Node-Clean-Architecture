59db43f4656ccb5648b0a4799032a53d60ef742e
Alberto Jose <albertojcvs@gmail.com>
2022-01-20 15:47:13 -0300

    chore: change the mongodb version to used at videos

--- src/infra/db/mongofb/helpers/mongo-helper.ts
+++ src/infra/db/mongofb/helpers/mongo-helper.ts
@@ -1,10 +1,12 @@
-import { MongoClient } from 'mongodb'
-import { disconnect } from 'process'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
   async connect () {
-    this.client = await MongoClient.connect(process.env.MONGO_URL)
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
   },
   async disconnect () {
     await this.client.close()
