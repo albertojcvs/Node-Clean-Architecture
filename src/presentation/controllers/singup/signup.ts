51cefad1d7c2a026951b077d35c00248c6943284
Alberto Jose <albertojcvs@gmail.com>
2022-01-26 13:24:10 -0300

    refactor: move passoword  comparation t the ValidationCompsite

--- src/presentation/controllers/singup/signup.ts
+++ src/presentation/controllers/singup/signup.ts
@@ -1,9 +1,8 @@
 import { AddAccount, Controller, HttpRequest, EmailValidator, HttpResponse, Validation } from './singup-protocols'
-import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
 
 export class SingUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator, private readonly addAccount: AddAccount, private readonly validation: Validation) {}
+  constructor (private readonly addAccount: AddAccount, private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -11,13 +10,7 @@ export class SingUpController implements Controller {
       if (error) {
         return badRequest(error)
       }
-      const { name, email, password, passwordConfirmation } = httpRequest.body
-
-      if (password !== passwordConfirmation) return badRequest(new InvalidParamError('passwordConfirmation'))
-
-      const isValid = this.emailValidator.isValid(email)
-
-      if (!isValid) return badRequest(new InvalidParamError('email'))
+      const { name, email, password } = httpRequest.body
 
       const account = await this.addAccount.add({ name, email, password })
       return ok(account)
