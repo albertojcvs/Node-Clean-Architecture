c023c12adcdfb6077be6a55d42a7ee34e0fe0cd2
Alberto Jose <albertojcvs@gmail.com>
2022-01-31 11:48:33 -0300

    feat: ensure DbLoadAccountByRepostory returns an account on success

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -8,7 +8,8 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   async load (accessToken: string, role?: string): Promise<AccountModel> {
     const token = await this.decrypter.decrypt(accessToken)
     if (token) {
-      await this.loadAccountByTokenRepository.loadByToken(token, role)
+      const account = await this.loadAccountByTokenRepository.loadByToken(token, role)
+      return account
     }
     return null
   }
