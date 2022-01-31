d417d48d49a34d66c74b24231f11cff8864564da
Alberto Jose <albertojcvs@gmail.com>
2022-01-31 13:19:04 -0300

    test: ensure AccountMongoRepository find an account  by tken without role

--- src/infra/db/mongofb/account/account-repository.spec.ts
+++ src/infra/db/mongofb/account/account-repository.spec.ts
@@ -72,4 +72,28 @@ describe('Account mongo repository', () => {
       expect(account.accessToken).toBe('any_token')
     })
   })
+
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken success', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+
+    test('Should return an null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
+  })
 })
