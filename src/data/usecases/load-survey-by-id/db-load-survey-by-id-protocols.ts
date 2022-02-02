8b443ed59e9abe37fc7341cced14031eb52c4088
Alberto Jose <albertojcvs@gmail.com>
2022-02-01 21:12:51 -0300

    refactor: add data usecases protocols files to simplify imports

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id-protocols.ts
@@ -0,0 +1,4 @@
+
+export * from '@/data/protocols/db/survey/load-survey-by-id'
+export * from '@/domain/models/survey'
+export * from '@/domain/usesCases/load-survey-by-id'
---
+++ src/data/usecases/load-surveys/db-load-surveys-protocols.ts
@@ -0,0 +1,3 @@
+export * from '@/domain/models/survey'
+export * from '@/domain/usesCases/load-surveys'
+export * from '@/data/protocols/db/survey/load-surveys-repository'
