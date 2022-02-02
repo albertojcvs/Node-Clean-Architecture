f7064e3ba5341860be34ee60cbc86bf3ebe3d0ab
Alberto Jose <albertojcvs@gmail.com>
2022-02-02 14:50:11 -0300

    feat: esnure saee survey result route returns 403 if no token is provided

---
+++ src/main/factories/useCases/survey-result/save=survey-result/db-save-survey-result-factory.ts
@@ -0,0 +1,7 @@
+import { DbSaveSurveyResult } from '@/data/usecases/survey-reuslt/save-survey-result/db-save-survey-result'
+import { SurveyResultMongoRepository } from '@/infra/db/mongodb/survey-result/survey-result-repository'
+
+export const makeDbSaveSurveyResult = (): DbSaveSurveyResult => {
+  const surveResultMongoRepository = new SurveyResultMongoRepository()
+  return new DbSaveSurveyResult(surveResultMongoRepository)
+}
